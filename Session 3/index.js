0964aad8f29fab98f625430d8699101b849ca182
Ahmed Hossam <128134339+Ahmed-Hossam-1@users.noreply.github.com>
2023-11-20 11:31:13 +0200

    first commit

---
+++ Session 3/index.js
@@ -0,0 +1,18 @@
+// HTTP Request
+
+const http = require("node:http");
+
+const server = http.createServer((req, res) => {
+  console.log("req", req.url);
+  if (req.url === "/") {
+    res.end("Home Page");
+  } else if (req.url === "/about") {
+    res.end("about Page");
+  } else {
+    res.end("Not Found Page");
+  }
+});
+
+server.listen(3001, () => {
+  console.log("Listen port 3001");
+});
